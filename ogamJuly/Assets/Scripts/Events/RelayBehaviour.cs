13c16a5c500f348f250d67eb2e76d362e7e6cbae
HannesHaggander <h.haggander@gmail.com>
2016-07-06 22:22:09 +0200

    Made a simple generator that generates a lot of events from the events resource folder

---
+++ ogamJuly/Assets/Scripts/Events/RelayBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RelayBehaviour : MonoBehaviour {
+
+    [SerializeField]
+    private float enterRadius = 5;
+    Collider[] inSphere;
+
+	void Update()
+    {
+        CheckForPlayer();
+    }
+
+    private void CheckForPlayer()
+    {
+        inSphere = Physics.OverlapSphere(transform.position, enterRadius);
+        if (inSphere.Length > 0)
+        {
+            foreach (Collider c in inSphere)
+            {
+                if (c.transform.root.tag.Equals("Player"))
+                {
+                    Debug.Log("End relay reached...");
+                    Application.LoadLevel(Application.loadedLevel);
+                }
+            }
+        }
+    }
+}
--- ogamJuly/Assets/Scripts/Space/Generators/GenerateEvents.cs
+++ ogamJuly/Assets/Scripts/Space/Generators/GenerateEvents.cs
@@ -6,13 +6,16 @@ public class GenerateEvents : MonoBehaviour {
     [SerializeField]
     private string prefabRelayPath = "Prefabs/Events/Relay";
     [SerializeField]
-    private string eventsFolder = "Prefabs/Events";
+    private string eventsFolder = "Prefabs/Events/GeneratedEvents";
     public float minRelayDistance = 5000, maxRelayDistance = 1000;
-    private 
+
+    [SerializeField]
+    private int maxEvents = 4;
 	
     void Start ()
     {
         CreateRelays();
+        GenerateRandomEvents();
 	}
 
     void CreateRelays()
@@ -23,6 +26,7 @@ public class GenerateEvents : MonoBehaviour {
                                               Random.Range(minRelayDistance, maxRelayDistance),
                                               0);
         GameObject endRelay = (GameObject)Instantiate(relayToSpawn, endRelayPosition, Quaternion.identity);
+        endRelay.AddComponent<RelayBehaviour>();
 
         Vector3 startToEnd = endRelayPosition - Vector3.zero;
         float angle = Mathf.Atan2(startToEnd.y, startToEnd.x) * Mathf.Rad2Deg;
@@ -32,12 +36,18 @@ public class GenerateEvents : MonoBehaviour {
 
     void GenerateRandomEvents()
     {
-
+        GameObject[] events = Resources.LoadAll<GameObject>("Prefabs/Events/GeneratedEvents");
+        foreach (GameObject g in events)
+        {
+            Debug.Log(g.name);
+        }
+        for(int i = 0; i < maxRelayDistance/100; i++)
+        {
+            for(int j = 0; j < maxEvents; j++)
+            {
+                Vector3 eventPosition = new Vector3(Random.Range(-500, 500), i * 100, 0);
+                Instantiate(events[Random.Range(0, events.Length)], eventPosition, Quaternion.identity);
+            }
+        }
     }
-	
-
-	void Update ()
-    {
-	
-	}
 }
