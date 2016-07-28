e7d7ab0f3db2af1ed0d47e36ce2b6f0d4632af1e
Ditchlol <hannes.haggander@gmail.com>
2016-07-28 23:53:59 +0200

    Found a bug where the player would not spawn, fixed that bug

--- ogamJuly/Assets/Scripts/Ship/ShipSceneVisability.cs
+++ ogamJuly/Assets/Scripts/Ship/ShipSceneVisability.cs
@@ -5,14 +5,36 @@ using System.Collections;
 public class ShipSceneVisability : MonoBehaviour {
 
     public GameObject ShipObject;
-    public GameObject spawnedShip;
     public string SpacePath = "Prefabs/Ships/PlayerShip";
 
     void OnLevelWasLoaded(int i)
     {
+        SpawnShip();
+    }
+
+    void Start()
+    {
+        SpawnShip();
+    }
+
+    void SpawnShip()
+    {
+        if (GameObject.FindGameObjectWithTag("Player"))
+        {
+            return;
+        }
         if (SceneManager.GetActiveScene().name.Equals("EventsMap"))
         {
-            Instantiate(Resources.Load(SpacePath));
+            Debug.Log("Spawning ship");
+            GameObject go = (GameObject)Resources.Load(SpacePath);
+            if (go)
+            {
+                Instantiate(go, Vector3.zero, Quaternion.identity);
+            }
+            else
+            {
+                Debug.Log("Failed loading ship " + SpacePath);
+            }
         }
     }
 }
