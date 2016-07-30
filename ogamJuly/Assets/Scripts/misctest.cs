39531519efc9cb08822a8e1792c971592993a522
HannesHaggander <h.haggander@gmail.com>
2016-07-31 00:14:42 +0200

    removed tmp file used for testing changing weapon

--- ogamJuly/Assets/Scripts/misctest.cs
+++ ogamJuly/Assets/Scripts/misctest.cs
@@ -4,19 +4,4 @@ using System.Collections;
 
 public class misctest : MonoBehaviour {
 
-    public ShipSceneVisability ssv;
-    public GameObject ssvObject;
-    public GameObject missileWeapon;
-    private string basePath = "Assets/Resources/";
-
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.U))
-        {
-            ssv = ssvObject.GetComponent<ShipSceneVisability>();
-            GameObject ship = ssv.GetActiveShip();
-            Modules mods = ship.transform.root.GetComponent<Modules>();
-            mods.ChangeWeapon(0, missileWeapon);
-        }
-    }
 }
