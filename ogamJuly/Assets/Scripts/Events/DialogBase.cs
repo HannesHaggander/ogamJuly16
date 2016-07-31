cfffd2703708b394ceee266eed2778f986b5b89c
HannesHaggander <h.haggander@gmail.com>
2016-07-31 13:23:30 +0200

    made a new dialog option, works better than the last one

---
+++ ogamJuly/Assets/Scripts/Events/DialogBase.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DialogBase : MonoBehaviour {
+
+    public float DialogTriggerDistance = 10;
+    protected DialogBehaviour DialBeh;
+    protected GameObject player;
+    public string dialogText = "";
+    public int numberOfResponses = 1;
+    public string[] responseTexts;
+
+    void InitiateDialog()
+    {
+        if (!player) { player = GameObject.FindGameObjectWithTag("Player"); }
+        if (!DialBeh) { DialBeh = GameObject.Find("UIDialog").GetComponent<DialogBehaviour>(); }
+    }
+
+    public void DialogUpdate()
+    {
+        if (player)
+        {
+            if (Vector3.Distance(transform.position, player.transform.position) < DialogTriggerDistance)
+            {
+                TriggerDialog();
+            }
+        }
+    }
+
+    virtual public void CreateDialog(string dialogTextParam, int nrOfResponses, string[] responseTextsParam)
+    {
+        if(nrOfResponses <= 0) { Debug.Log("<< number of responses is less or equal to 0 >> "); return; }
+
+        dialogText = dialogTextParam;
+        numberOfResponses = nrOfResponses;
+        responseTexts = responseTextsParam;
+        if (DialBeh)
+        {
+            DialBeh.SetSettings(dialogText, numberOfResponses, responseTexts, gameObject);
+        }
+        else
+        {
+            Debug.Log("Dialog Behaviour missing from " + gameObject.name);
+        }
+
+    }
+
+    virtual public void TriggerDialog()
+    {
+        Debug.Log("Triggered Dialog needs to be overwritten for " + gameObject.name);
+    }
+
+    virtual public void Response1()
+    {
+        Debug.Log("Response 1 needs to be overwritten for " + gameObject.name);
+    }
+
+    virtual public void Response2()
+    {
+        Debug.Log("Response 2 needs to be overwritten for " + gameObject.name);
+    }
+
+    virtual public void Response3()
+    {
+        Debug.Log("Response 3 needs to be overwritten for " + gameObject.name);
+    }
+}
+
---
+++ ogamJuly/Assets/Scripts/UserInterface/DialogBehaviour.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class DialogBehaviour : MonoBehaviour {
+
+    public bool dialogActive = false;
+
+    public Text dialogText;
+
+    public Button resp1;
+    public Button resp2;
+    public Button resp3;
+
+    public Button hide;
+
+    public void ToggleVisability()
+    {
+        foreach(Transform t in transform)
+        {
+            if (!t.gameObject.name.Equals("Cancel"))
+            {
+                dialogActive = !t.gameObject.activeSelf;
+                t.gameObject.SetActive(!t.gameObject.activeSelf);
+            }
+        }
+    }
+
+    void ToggleVisability(bool b)
+    {
+        dialogActive = b;
+        foreach(RectTransform rt in transform)
+        {
+            rt.gameObject.SetActive(b);
+        }
+    }
+
+    public void SetSettings(string dialogTextParam, int nrOfResponses, string[] responseTexts, GameObject origin)
+    {
+        resp1.gameObject.SetActive(false);
+        resp2.gameObject.SetActive(false);
+        resp3.gameObject.SetActive(false);
+
+        if (!origin) { Debug.Log("source missing "); return; }
+        if(nrOfResponses != responseTexts.Length) { Debug.Log("string[] failure length" + nrOfResponses + " != " + (responseTexts.Length)); return; }
+
+       // DialogBase dialbase = origin.GetComponent<DialogBase>();
+
+        dialogText.text = dialogTextParam;
+        for(int i = 0; i < nrOfResponses; i++)
+        {
+            switch (i)
+            {
+                case 0: resp1.gameObject.SetActive(true);
+                    Text resp1Text = resp1.GetComponentInChildren<Text>();
+                    if (resp1Text){ resp1Text.text = responseTexts[i]; }
+                    break;
+
+                case 1: resp2.gameObject.SetActive(true);
+                    Text resp2Text = resp2.GetComponentInChildren<Text>();
+                    if (resp2Text) { resp2Text.text = responseTexts[i]; }
+                    break;
+
+                case 2: resp3.gameObject.SetActive(true);
+                    Text resp3Text = resp3.GetComponentInChildren<Text>();
+                    if (resp3Text) { resp3Text.text = responseTexts[i]; }
+                    break;
+
+                default: Debug.Log("Something went horribly wrong... "); break;
+            }
+        }
+    }
+}
