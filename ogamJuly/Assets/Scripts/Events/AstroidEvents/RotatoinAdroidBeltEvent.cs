6f9cc05f9b4c9787b57e3303a93ffac1850526ba
HannesHaggander <h.haggander@gmail.com>
2016-08-02 12:40:29 +0200

    made a rotating astroidbelt

--- ogamJuly/Assets/Scripts/Events/AstroidEvents/RotatoinAdroidBeltEvent.cs
+++ ogamJuly/Assets/Scripts/Events/AstroidEvents/RotatoinAdroidBeltEvent.cs
@@ -3,13 +3,34 @@ using System.Collections;
 
 public class RotatoinAdroidBeltEvent : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public GameObject[] astroidObjects;
+
+    public float RotationSpeed = 10;
+
+    private float rotValue = 0;
+
+    void Start()
+    {
+        int randomValue = Random.Range(0, 2);
+        
+        if(randomValue == 0)
+        {
+            RotationSpeed = -1 * RotationSpeed;
+        }
+
+        GameObject tmpGO;
+        foreach(Transform t in transform)
+        {
+            tmpGO = Instantiate(astroidObjects[Random.Range(0, astroidObjects.Length)], 
+                                t.transform.position, 
+                                Quaternion.Euler(0,0,Random.Range(0,360))) as GameObject;
+            tmpGO.transform.SetParent(t);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        transform.rotation = Quaternion.Euler(0, 0, rotValue);
+        rotValue += RotationSpeed * Time.deltaTime;
+    }
 }
